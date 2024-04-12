c7988ae0c7471f7a69a7d085fcf60652e26bd223
ggmun <ggmun@lfit.co.kr>
2024-04-12 18:03:27 +0900

    파일 누락한거 push

---
+++ src/views/board/VueTest.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>{{ testData }}</h1>
+    <h1>프론트</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "VueTest",
+  components: [],
+  data() {
+    return {
+      testData: "",
+    };
+  },
+  methods: {
+    get() {
+      this.axios.get("/api/testVue").then((response) => {
+        this.testData = response.data;
+      });
+    },
+  },
+  mounted() {
+    this.get();
+  },
+};
+</script>
