6190cb3644bbe32f2da2eddf61971254867858b8
ggmun <ggmun@lfit.co.kr>
2024-04-17 17:59:49 +0900

    vue.config.js 원복, camelCase 적용 완료

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  // npm run serve 개발 진행시에 포트가 다르기때문에 프록시 설정
+  // 개발 서버 설정
   devServer: {
-      // 프록시 설정
-        proxy: {
-        '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      }
+    // 개발 서버 포트 설정
+    port: 3030, // 원하는 포트로 변경 가능
+
+    // 개발 서버 호스트 설정 (로컬 호스트 사용)
+    host: 'localhost',
+
+    // CORS 관련 설정은 따로 추가할 필요 없음
     }
-  }
 })
