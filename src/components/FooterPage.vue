94bd3aa81b7109c57af6ea14a998f47f07409329
ggmun <ggmun@lfit.co.kr>
2024-05-23 18:02:38 +0900

    footer 디자인 수정

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,8 +1,12 @@
 <template>
-  <hr/>
-  <footer>
-    고객센터<br/>
-    문의 이메일 : test@gmail.com
+  <footer class="text-body-secondary py-5">
+    <div class="container">
+      <p class="float-end mb-1">
+        <a href="#">Back to top</a>
+      </p>
+      <p class="mb-1">Copyright © www.domain.com All rights reserved.</p>
+      <p class="mb-0">사이트 운영팀: help@domain.com | <a href="">이용약관</a> | <a href="">개인정보처리방침</a> | <a href="#">청소년보호정책</a> | <a href="">문의/신고</a> | <a href="">게시글 중단 요청</a></p>
+    </div>
   </footer>
 </template>
 
